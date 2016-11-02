f2af827b76933b60135d71358f9625268ca4b70e
Mathias Havdal <matte3560@gmail.com>
2016-11-02 17:55:42 +0100

    Start work to implement platform driver for gamepad.

--- driver-gamepad.c
+++ driver-gamepad.c
@@ -1,13 +1,52 @@
 /*
- * This is a demo Linux kernel module.
+ * This is a gamepad Linux kernel module.
  */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+
+
+static int gamepad_probe(struct platform_device *p_dev) {
+	printk("Device found for gamepad driver\n");
+
+	// Allocate device number
+	dev_t dev;
+	int result;
+	result = alloc_chrdev_region(&dev, 0, 1, "gamepad");
+	if (result != 0) {
+		return -1; // Failed to allocate device number
+	}
+	printk("Device number allocated: major %i, minor %i\n", MAJOR(dev), MINOR(dev));
+	
+	return 0;
+}
+
+static int gamepad_remove(struct platform_device *p_dev) {
+	return 0;
+}
+
+static const struct of_device_id gamepad_of_match[] = {
+	{ .compatible = "tdt4258", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, gamepad_of_match);
+
+static struct platform_driver gamepad_driver = {
+	.probe = gamepad_probe,
+	.remove = gamepad_remove,
+	.driver = {
+		.name = "gamepad",
+		.owner = THIS_MODULE,
+		.of_match_table = gamepad_of_match,
+	},
+};
 
 /*
- * template_init - function to insert this module into kernel space
+ * gamepad_init - function to insert this module into kernel space
  *
  * This is the first of two exported functions to handle inserting this
  * code into a running kernel
@@ -15,27 +54,31 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int __init template_init(void)
+static int __init gamepad_init(void)
 {
 	printk("Hello World, here is your module speaking\n");
+
+	// Register platform driver
+	platform_driver_register(&gamepad_driver);
+
 	return 0;
 }
 
 /*
- * template_cleanup - function to cleanup this module from kernel space
+ * gamepad_cleanup - function to cleanup this module from kernel space
  *
  * This is the second of two exported functions to handle cleanup this
  * code from a running kernel
  */
 
-static void __exit template_cleanup(void)
+static void __exit gamepad_cleanup(void)
 {
 	 printk("Short life for a small module...\n");
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+module_init(gamepad_init);
+module_exit(gamepad_cleanup);
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+MODULE_DESCRIPTION("Module for accessing gamepad buttons.");
 MODULE_LICENSE("GPL");
 
