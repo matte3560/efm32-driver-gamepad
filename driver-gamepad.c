ebac9209e6acf0fa2d8c8f4c70c2ab681cd9ea63
Mathias Havdal <matte3560@gmail.com>
2016-11-04 16:47:07 +0100

    Start work on implementing interrupts.

--- driver-gamepad.c
+++ driver-gamepad.c
@@ -8,15 +8,28 @@
 #include <linux/platform_device.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/interrupt.h>
+#include <linux/ioport.h>
+#include <asm/io.h>
+
+#include "offsets.h"
+
+// Device name
+#define DEVICE_NAME "gamepad"
 
 // Device number used for gamepad
-dev_t gamepad_dev;
+static dev_t gamepad_dev;
 
 // cdev struct
-struct cdev gamepad_cdev;
+static struct cdev gamepad_cdev;
 
 // Class struct
-struct class *gamepad_cl;
+static struct class *gamepad_cl;
+
+// Platfom information
+static struct resource *gamepad_res;
+static int gamepad_irq_even;
+static int gamepad_irq_odd;
 
 
 
@@ -54,25 +67,60 @@ static struct file_operations gamepad_fops = {
 };
 
 
+// Interrupt handler
+static irqreturn_t gamepad_irq_handler(int irq, void *dev_id) {
+	printk("Handled interrupt %i\n", ioread32((uint32_t*)(gamepad_res->start + OFF_GPIO_PC_DIN)));
+
+	iowrite32(ioread32((uint32_t*)(gamepad_res->start + OFF_GPIO_IF)), (uint32_t*)(gamepad_res->start + OFF_GPIO_IFC));
+
+	return IRQ_HANDLED;
+}
+
+
 static int gamepad_probe(struct platform_device *p_dev) {
 	int result;
 
 	printk("Device found for gamepad driver\n");
 
+	// Get platform info
+	gamepad_res = platform_get_resource(p_dev, IORESOURCE_MEM, 0);
+	printk("Name: %s\n", gamepad_res->name);
+	printk("Base addr: %x\n", gamepad_res->start);
+	gamepad_irq_even = platform_get_irq(p_dev, 0);
+	gamepad_irq_odd = platform_get_irq(p_dev, 1);
+	printk("Interrupt even: %i, odd: %i\n", gamepad_irq_even, gamepad_irq_odd);
+
+	// Configure GPIO buttons
+	iowrite32(0x33333333, (uint32_t*)(gamepad_res->start + OFF_GPIO_PC_MODEL));
+	iowrite32(0xFF, (uint32_t*)(gamepad_res->start + OFF_GPIO_PC_DOUT));
+
+	// Configure GPIO interrutps
+	iowrite32(0x22222222, (uint32_t*)(gamepad_res->start + OFF_GPIO_EXTIPSELL));
+	iowrite32(0xff, (uint32_t*)(gamepad_res->start + OFF_GPIO_EXTIFALL));
+	iowrite32(0xff, (uint32_t*)(gamepad_res->start + OFF_GPIO_EXTIRISE));
+	iowrite32(0xff, (uint32_t*)(gamepad_res->start + OFF_GPIO_IEN));
+
+	// Enable interrupts
+	result = request_irq(gamepad_irq_even, (irq_handler_t)gamepad_irq_handler,
+			0, DEVICE_NAME, 0);
+	if (result != 0) return -1; // Failed to set up interrupts
+	result = request_irq(gamepad_irq_odd, (irq_handler_t)gamepad_irq_handler,
+			0, DEVICE_NAME, 0);
+	if (result != 0) return -1; // Failed to set up interrupts
+
 	// Allocate device number
-	result = alloc_chrdev_region(&gamepad_dev, 1, 1, "gamepad");
-	if (result != 0) {
-		return -1; // Failed to allocate device number
-	}
+	result = alloc_chrdev_region(&gamepad_dev, 1, 1, DEVICE_NAME);
+	if (result < 0) return -1; // Failed to allocate device number
 	printk("Device number allocated: major %i, minor %i\n", MAJOR(gamepad_dev), MINOR(gamepad_dev));
 
 	// Initialize cdev
 	cdev_init(&gamepad_cdev, &gamepad_fops);
-	cdev_add(&gamepad_cdev, gamepad_dev, 1);
+	result = cdev_add(&gamepad_cdev, gamepad_dev, 1);
+	if (result < 0) return -1; // Failed to add cdev
 
 	// Make visible in userspace
-	gamepad_cl = class_create(THIS_MODULE, "gamepad");
-	device_create(gamepad_cl, NULL, gamepad_dev, NULL, "gamepad");
+	gamepad_cl = class_create(THIS_MODULE, DEVICE_NAME);
+	device_create(gamepad_cl, NULL, gamepad_dev, NULL, DEVICE_NAME);
 	
 	return 0;
 }
@@ -101,7 +149,7 @@ static struct platform_driver gamepad_driver = {
 	.probe = gamepad_probe,
 	.remove = gamepad_remove,
 	.driver = {
-		.name = "gamepad",
+		.name = DEVICE_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = gamepad_of_match,
 	},
